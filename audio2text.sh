b31d3ce9586106319ea4eddd3951c11588222386
cesine <cesine@host>
2011-10-06 17:05:33 -0400

    changed pathnames

--- audio2text.sh
+++ audio2text.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+!/bin/bash
 if [ -z "$1" ]; then 
   echo usage: $0 directory "Provide a file name (without the .srt, .amr, .wav suffixes)"
    exit
@@ -6,11 +6,11 @@ fi
 echo "======================================================="
 
 echo ==Moving to data folder
-cd ../nodejs-pocketsphinxdata
+cd ../data
 
-touch ../nodejs-pocketsphinxtemp/$1_server.srt
+touch ../temp/$1_server.srt
 echo ==Creating files _client, _server and .srt
-cp ../nodejs-pocketsphinxtemp/$1_client.srt $1.srt
+cp ../temp/$1_client.srt $1.srt
 
 echo ==Branching to user branch
 git checkout User
@@ -22,24 +22,24 @@ git checkout MachineTranscription
 git merge User
 
 echo ==Converting mp3/amr $1.amr to pcm/wav $1.wav
-ffmpeg -y -i ../nodejs-pocketsphinxtemp/$1.amr ../nodejs-pocketsphinxtemp/$1.wav
+ffmpeg -y -i ../temp/$1.amr ../temp/$1.wav
 
 echo ==Running pocketsphinx
 echo "0:00:00.020,0:00:00.020\nResults of the machine transcription will appear below when ready.\n\n" >> $1.srt
-#170word gramamr: java -jar ../nodejs-pocketsphinx/sphinx4files/transcriber/bin/Transcriber.jar ../nodejs-pocketsphinxtemp/$1.wav 2>&1 | tee -a $1.srt 
-java -jar /home/gina/.groovy/lib/sphinx4/bin/LatticeDemo.jar ../nodejs-pocketsphinxtemp/$1.wav | grep "I heard:" | sed -e 's/I heard://' 2>&1 | tee -a $1.srt 
+#170word gramamr: java -jar ..//sphinx4files/transcriber/bin/Transcriber.jar ../temp/$1.wav 2>&1 | tee -a $1.srt 
+java -jar /home/gina/.groovy/lib/sphinx4/bin/LatticeDemo.jar ../temp/$1.wav | grep "I heard:" | sed -e 's/I heard://' 2>&1 | tee -a $1.srt 
 
-cp $1.srt ../nodejs-pocketsphinxtemp/$1_server.srt
-#cd ../nodejs-pocketsphinx/testinstallpocketsphinx
-#./hello_ps goforward.raw | grep Recognized >> ../../nodejs-pocketsphinxdata/$1
-#cd ../../nodejs-pocketsphinxdata
+cp $1.srt ../temp/$1_server.srt
+#cd ..//testinstallpocketsphinx
+#./hello_ps goforward.raw | grep Recognized >> ../../data/$1
+#cd ../../data
 
 echo "==Committing new transcripion"
 git add *.srt
 git commit -m "ran pocketsphinx on $1"
 
 echo "==Processing prosody with Praat"
-praat ../nodejs-pocketsphinx/praatfiles/praat-script-syllable-nuclei-v2file.praat -25 2 0.3 yes /home/gina/aublog/nodejs-pocketsphinxworkspace/nodejs-pocketsphinxtemp $1.wav 2>&1 | tee -a praatresults.csv
+praat ..//praatfiles/praat-script-syllable-nuclei-v2file.praat -25 2 0.3 yes /home/gina/aublog/workspace/temp $1.wav 2>&1 | tee -a praatresults.csv
 
 echo "==Commiting new acoustic results"
 git add praatresults.csv
@@ -48,6 +48,6 @@ git commit -m "ran praat on $1"
 echo "==Server transcription is ready."
 #git checkout master #leave it in the MachineTranascription branch so that the node will copy the right version of the file into the server's response.
 
-cd ../nodejs-pocketsphinx
+cd ../
 
 echo "==============================================================="
