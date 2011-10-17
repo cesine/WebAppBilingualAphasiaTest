a27af6dc07b0ae78b206cf643fb3b6ef68f7ed81
cesine <cesine@host>
2011-10-16 23:13:25 -0400

    using git and ffmpeg which comes in the repository

--- audio2text.sh
+++ audio2text.sh
@@ -8,26 +8,25 @@ echo "======================================================="
 echo ==Moving to backup folder
 cd ../backup
 
-touch ../results/$1_server.srt
 echo ==Creating files _client, _server and .srt
-cp ../results/$1_client.srt $1.srt
+cp ../results/$1.3gp .
 
 echo ==Branching to user branch
-git checkout User
-git add *.srt
-git commit -m "added file $1 from user"
+../src/dependancies/git checkout tabletclient
+../src/dependancies/git add *.srt
+../src/dependancies/git commit -m "added file $1 from user"
 
 echo ==Branching to machinetranscription branch
-git checkout MachineTranscription
-git merge User
+../src/dependancies/git checkout laptopserver
+../src/dependancies/git merge tabletclient
 
 echo ==Converting $1.3gp video to pcm/wav $1.wav
-ffmpeg -y -i ../results/$1.3gp ../results/$1.wav
+../src/dependancies/ffmpeg -y -i ../results/$1.3gp ../results/$1.wav
 
-echo ==Running pocketsphinx
-echo "0:00:00.020,0:00:00.020\nResults of the machine transcription will appear below when ready.\n\n" >> $1.srt
+#echo ==Running pocketsphinx
+#echo "0:00:00.020,0:00:00.020\nResults of the machine transcription will appear below when ready.\n\n" >> $1.srt
 #170word gramamr: java -jar ../src/sphinx4files/transcriber/bin/Transcriber.jar ../results/$1.wav 2>&1 | tee -a $1.srt 
-java -jar /home/gina/.groovy/lib/sphinx4/bin/LatticeDemo.jar ../results/$1.wav | grep "I heard:" | sed -e 's/I heard://' 2>&1 | tee -a $1.srt 
+#java -jar /home/gina/.groovy/lib/sphinx4/bin/LatticeDemo.jar ../results/$1.wav | grep "I heard:" | sed -e 's/I heard://' 2>&1 | tee -a $1.srt 
 
 cp $1.srt ../results/$1_server.srt
 #cd ../src/testinstallpocketsphinx
@@ -35,15 +34,15 @@ cp $1.srt ../results/$1_server.srt
 #cd ../../backup
 
 echo "==Committing new transcripion"
-git add *.srt
-git commit -m "ran pocketsphinx on $1"
+../src/dependancies/git add *.srt
+../src/dependancies/git commit -m "ran pocketsphinx on $1"
 
 echo "==Processing prosody with Praat"
-#praat ../src/praatfiles/praat-script-syllable-nuclei-v2file.praat -25 2 0.3 yes /home/gina/aublog/workspace/results $1.wav 2>&1 | tee -a praatresults.csv
+/Applications/Praat.app/Contents/MacOS/Praat ../src/praatfiles/praat-script-syllable-nuclei-v2file.praat -25 2 0.3 yes OPrimeAdministrator.app/Contents/Resources/backup $1.wav 2>&1 | tee -a praatresults.csv
 
 echo "==Commiting new acoustic results"
-git add praatresults.csv
-git commit -m "ran praat on $1"
+../src/dependancies/git add praatresults.csv
+../src/dependancies/git commit -m "ran praat on $1"
 
 echo "==Server transcription is ready."
 #git checkout master #leave it in the MachineTranascription branch so that the node will copy the right version of the file into the server's response.
